5f17fabbd508f5f115ac0a05bae5ed8dda72b8d6
Paul Mureithi <mureithipaul999@.com>
2025-07-21 10:37:45 +0300

    first commit

---
+++ account/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+# login form
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+# registartion form
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    # validation to check if the passwords match
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password2']
+    
+    
---
+++ account/views.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import LoginForm, UserRegistrationForm
+
+# this is the login view.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username = cd['username'],
+                password = cd['password']
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated Succesfully')
+                else:
+                    return HttpResponse('Disabled Account.')
+            else:
+                return HttpResponse('Invalid Login.')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form':form})
+
+# the dashboard view
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
+
+# this is the registration view
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # create a new user object, but don't save it
+            new_user = user_form.save(commit=False)
+            # set the chosen password & perfom password hashing before storage
+            new_user.set_password(user_form.cleaned_data['password'])
+            # save the user object
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user':new_user})
+    else:
+        user_form =UserRegistrationForm()
+    return render (request, 'account/register.html', {'user_form':user_form})
